8bc27cf65b8fb0e9f48282d35579c2c12339f9e9
SajanThapa22 <sajanthapa1888@gmail.com>
2024-06-04 21:42:29 +0545

    recent activity

---
+++ src/components/RecentActivity.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import User from "./User";
+
+const RecentActivity = () => {
+  return (
+    <div className="flex justify-between bg-bg-comp rounded-[18px]">
+      <User />
+      <div className="capitalize text-[#68676E]">follow</div>
+    </div>
+  );
+};
+
+export default RecentActivity;
