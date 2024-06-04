7b4368ec4ae11bf9cf218ff3c7ad307c23afb56b
SajanThapa22 <sajanthapa1888@gmail.com>
2024-06-04 21:34:48 +0545

    customer details

---
+++ src/components/User.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const User = () => {
+  return (
+    <div className="flex">
+      <div className="w-9 aspect-square rounded-full overflow-hidden">
+        <img src="" alt="" />
+      </div>
+
+      <div className="flex flex-col justify-between">
+        <p className="">name</p>
+        <p className="">text</p>
+      </div>
+    </div>
+  );
+};
+
+export default User;
