1b421bd85daca6fdc2499ff52f99768ebbf04b24
SajanThapa22 <sajanthapa1888@gmail.com>
2024-07-03 23:41:52 +0545

    trending bid grid

--- src/sections/TrendingBids.tsx
+++ src/sections/TrendingBids.tsx
@@ -15,7 +15,11 @@ const TrendingBids = () => {
           <p className="capitalize text-txt-clr text-sm">book</p>
         </div>
       </div>
-      <div className="grid gap-7 md:gap-8 lg:gap-10 xl:gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <BidCards />
+        <BidCards />
+        <BidCards />
+        <BidCards />
         <BidCards />
         <BidCards />
         <BidCards />
