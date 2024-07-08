32e772f35f0609bfc6cb76a8bdeec5f64d427161
SajanThapa22 <sajanthapa1888@gmail.com>
2024-07-08 21:38:27 +0545

    adding chevron to the title section

--- src/components/WelomeTitle.tsx
+++ src/components/WelomeTitle.tsx
@@ -1,3 +1,5 @@
+import Chevron from "../assets/svgs/chevron.svg?react";
+
 interface Props {
   pageTitle: string;
   titleBelow: string;
@@ -9,9 +11,13 @@ const WelomeTitle = ({ pageTitle, titleBelow }: Props) => {
       <p className="text-[24px] font-semibold mb-[5px] 2xl:mb-[10px]">
         {pageTitle}
       </p>
-      <div className="text-sm 2xl:text-[22px] w-full flex justify-between">
+      <div className="text-sm 2xl:text-[22px] w-full flex items-center justify-between">
         <p>{`Welcome ${titleBelow} Page`}</p>
-        <p>Home Bids</p>
+        <div className="flex gap-[10px] items-center">
+          <p>home</p>
+          <Chevron className="text-txt-clr" />
+          <p>{titleBelow}</p>
+        </div>
       </div>
     </div>
   );
