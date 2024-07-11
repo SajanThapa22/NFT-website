26e7f7b8bfbc6876bdc7c5306fa75fa21f13fa8c
SajanThapa22 <sajanthapa1888@gmail.com>
2024-07-11 23:01:22 +0545

    welcome title fix

--- src/components/WelomeTitle.tsx
+++ src/components/WelomeTitle.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Chevron from "../assets/svgs/chevron.svg?react";
 
 interface Props {
@@ -6,12 +7,18 @@ interface Props {
 }
 
 const WelomeTitle = ({ pageTitle, titleBelow }: Props) => {
+  const [width, setWidth] = useState(window.innerWidth);
+  window.addEventListener("resize", () => setWidth(window.innerWidth));
   return (
     <div className="capitalize text-txt-clr mt-[50px] mb-10">
       <p className="text-[24px] font-semibold mb-[5px] 2xl:mb-[10px]">
         {pageTitle}
       </p>
-      <div className="text-sm 2xl:text-[22px] w-full flex items-center justify-between">
+      <div
+        className={`text-sm 2xl:text-[22px] w-full flex items-center justify-between ${
+          width < 340 ? "flex-col items-baseline gap-2" : "flex-row"
+        }`}
+      >
         <p>{`Welcome ${titleBelow} Page`}</p>
         <div className="flex gap-[10px] items-center">
           <p className="text-[#6C7AA0]">home</p>
