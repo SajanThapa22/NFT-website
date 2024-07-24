18a31311817e9bd7b91f224fea651e26aebe4a17
SajanThapa22 <sajanthapa1888@gmail.com>
2024-07-24 21:30:42 +0545

    navicon

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/nft-website/",
+  base: "/nft-website/",
   plugins: [react(), svgr()],
 });
